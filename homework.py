deb93587360a14b6933b9a715cca6db3c248182e
nergis13 <nergisbayar@outlook.com>
2024-08-06 06:42:15 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,59 @@
+
+guess = input("Please enter a number : ")
+numbers = ["0","1","2","3","4","5","6","7","8","9"]
+
+
+
+if guess in numbers:
+    print("Rakamdir")
+else:
+    print("Rakam degildir.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
